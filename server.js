769f1d2a52d377ea7d929605e02d864998184796
SANSAI-FSD <SANSAISANSAI2003@GMAIL.COM>
2025-12-11 22:51:16 +0530

    cors

--- server.js
+++ server.js
@@ -23,15 +23,46 @@ connectDB();
 // Middlewares
 app.use(express.json());
 
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       callback(null, origin); // allow all origins safely
+//     },
+//     credentials: true,
+    
+//   })
+// );
+
 app.use(
   cors({
-    origin: "true",
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // allow mobile apps, Postman, React SSR
+      return callback(null, origin);            // allow frontend domains
+    },
     credentials: true,
-    
   })
 );
 
 
+
+
+// app.use(
+//   session({
+//     secret: "supersecretkey",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: MongoStore.create({
+//       mongoUrl: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/companyApp",
+//       ttl: 14 * 24 * 60 * 60, // 14 days
+//     }),
+//     cookie: {
+//       httpOnly: true,
+//       maxAge: 14 * 24 * 60 * 60 * 1000,
+//       secure: false, // true only in https
+//       sameSite: "lax",
+//     },
+//   })
+// );
 
 app.use(
   session({
@@ -45,11 +76,12 @@ app.use(
     cookie: {
       httpOnly: true,
       maxAge: 14 * 24 * 60 * 60 * 1000,
-      secure: false, // true only in https
-      sameSite: "lax",
+      secure: true,      // because Render backend uses HTTPS
+      sameSite: "none",  // REQUIRED for Vercel/Netlify frontend
     },
   })
 );
+
 
 // Routes
 app.use("/api/auth", authRoutes);
